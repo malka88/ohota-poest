7f7dad8d214396de8efde137d345feb3881de798
malka88 <malka_88@outlook.com>
2022-04-02 13:16:39 +0700

    add react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Footer, Header } from './containers';
+import { Navbar } from './components';
+
+const App = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default App
